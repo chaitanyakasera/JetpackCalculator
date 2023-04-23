cb430850266ca9fda0fca4dd9ee4647b0c3ac428
Chaitanya Kasera <chaitanyakasera1@gmail.com>
2023-04-23 14:54:03 +0530

    compile error resolved

---
+++ app/src/main/java/com/cnm/jetpackcalculator/compose/Composables.kt
@@ -0,0 +1,15 @@
+package com.cnm.jetpackcalculator.compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+
+fun CalculatorButton(
+    symbol: String,
+    buttonPadding: Dp,
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+
+}
